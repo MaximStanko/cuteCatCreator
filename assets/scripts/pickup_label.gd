a875e2d43b21549906d20780f5b0af94cff0768d
Tiramiezu <tiramiezu@protonmail.com>
2026-06-20 20:27:21 +0200

    misc

--- assets/scripts/pickup_label.gd
+++ assets/scripts/pickup_label.gd
@@ -1,15 +1,17 @@
 extends Node
 
+var detector
 
 func _ready() -> void:
-	self.visible = false
+	self.text = "?"
+	detector = get_parent()
 
 
 func _on_pickup_detector_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
-		self.visible = true
+		self.text = "!"
 
 
 func _on_pickup_detector_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
-		self.visible = false
+		self.text = detector.pickupName
