10032fe69950d2a11839d8269a83aa82e6513f5b
MaximStanko <f.maxim.stanko@gmail.com>
2026-06-20 18:03:01 +0200

    cat pot

---
+++ assets/scripts/pot_vertex.gd
@@ -0,0 +1,8 @@
+extends MeshInstance3D
+
+@export var vertex_index: int
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			print("pressed " + str(vertex_index))
