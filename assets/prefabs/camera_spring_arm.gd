f222ab4bc40e8440e1e920eb7cfb61a5c085d71e
3dot141592 <robin.luepke@gmail.com>
2026-06-20 18:19:56 +0200

    Stuff n shit

--- assets/prefabs/camera_spring_arm.gd
+++ assets/prefabs/camera_spring_arm.gd
@@ -1,9 +1,12 @@
 extends SpringArm3D
 
 @export var mouse_sensibility: float = 0.005
+@export_range(-80.0, -5.0, 0.1, "radians_as_degrees") var min_vertical_angle: float = deg_to_rad(-60.0)
+@export_range(-5.0, 20.0, 0.1, "radians_as_degrees") var max_vertical_angle: float = 0.0
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	rotation.x = clamp(rotation.x, min_vertical_angle, max_vertical_angle)
 
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -12,4 +15,4 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotation.y = wrapf(rotation.y, 0.0, TAU)
 		
 		rotation.x -= event.relative.y * mouse_sensibility
-		rotation.x = clamp(rotation.x, -PI/2, PI/4)
+		rotation.x = clamp(rotation.x, min_vertical_angle, max_vertical_angle)
