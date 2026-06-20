5a8ee5e8183ad4b0d48b7bf96d0f4ba15e7892aa
Tönjes <toenjes.mannhardt@student.hpi.uni-potsdam.de>
2026-06-20 20:37:13 +0200

    pause & main menu (and removed broken ground node)

---
+++ assets/scripts/pause_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+func _ready() -> void:
+	hide()
+	$resumeButton.pressed.connect(_on_play)
+	$quitButton.pressed.connect(_on_quit)
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		print("pausinggggg")
+		toggle_pause()
+
+func toggle_pause() -> void:
+	var paused := not get_tree().paused
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().paused = paused
+	visible = paused
+
+func _on_play() -> void:
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	hide()
+	
+func _on_quit() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
