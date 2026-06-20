9a2e1da6087c76da4ac3428b68c81bf2dcff04eb
Tiramiezu <tiramiezu@protonmail.com>
2026-06-20 18:50:39 +0200

    reimplement 3d pickup points

--- assets/scripts/pickup_detector.gd
+++ assets/scripts/pickup_detector.gd
@@ -2,6 +2,18 @@ extends Area3D
 
 @export var pickupName: String
 
+var player_inside = false
+
 func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
-		print(pickupName)
+		player_inside = true
+
+func _on_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("player"):
+		player_inside = false
+		
+func _unhandled_key_input(event: InputEvent) -> void:
+	if not event.is_pressed() or event.is_echo() or !player_inside:
+		return
+	match (event as InputEventKey).physical_keycode:
+		KEY_E: Inventory.set_held_item(pickupName)
