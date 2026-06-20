1d62a50207fcbdb2a9bf70b72302227e2398e986
3dot141592 <robin.luepke@gmail.com>
2026-06-21 00:08:57 +0200

    Meow

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,17 +1,29 @@
 extends CharacterBody3D
 
 
-const SPEED = 7.0
+const SPEED = 4.0
 const JUMP_VELOCITY = 4.5
 
 @export var turn_speed: float = 12.0
 @export var model_forward_offset: float = 0.0
+@export var run_animation: StringName = &"Player Animationen/Rennen"
+@export var idle_animation: StringName = &"Player Animationen/Chill"
+@export var jump_start_animation: StringName = &"Player Animationen/jump_start"
+@export var jump_idle_animation: StringName = &"Player Animationen/jump_idle"
+@export var animation_blend_time: float = 0.15
 
 @onready var camera: Camera3D = $SpringArm3D/Camera3D
 @onready var model: Node3D = $Mage
+@onready var animation_player: AnimationPlayer = $Mage/AnimationPlayer
+
+
+func _ready() -> void:
+	_play_animation(idle_animation)
 
 
 func _physics_process(delta: float) -> void:
+	var jumped := false
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,6 +31,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumped = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -35,9 +48,34 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		var target_rotation := atan2(direction.x, direction.z) + model_forward_offset
-		model.rotation.y = lerp_angle(model.rotation.y, target_rotation, turn_speed * delta)
+		model.global_rotation.y = lerp_angle(model.global_rotation.y, target_rotation, turn_speed * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+	var now_on_floor := is_on_floor()
+	if jumped:
+		_play_animation(jump_start_animation)
+	elif not now_on_floor:
+		if not _is_animation_playing(jump_start_animation):
+			_play_animation(jump_idle_animation)
+	else:
+		if direction:
+			_play_animation(run_animation)
+		else:
+			_play_animation(idle_animation)
+
+
+func _play_animation(animation_name: StringName) -> void:
+	if animation_player.current_animation == animation_name and animation_player.is_playing():
+		return
+	if not animation_player.has_animation(animation_name):
+		return
+
+	animation_player.play(animation_name, animation_blend_time)
+
+
+func _is_animation_playing(animation_name: StringName) -> bool:
+	return animation_player.current_animation == animation_name and animation_player.is_playing()
