8fe087655b5be528e9fe8a6a874b88fd8daf51d9
Tiramiezu <tiramiezu@protonmail.com>
2026-06-21 14:37:00 +0200

    sadcat

--- assets/scripts/cat_animator.gd
+++ assets/scripts/cat_animator.gd
@@ -1,12 +1,9 @@
 extends Node3D
 
-var cat_rhombus: PackedScene = preload("res://assets/prefabs/cats/hexen_gatze.tscn")
-
 func summon(shape):
-	if shape == "rhombus":
-		var rhombus_katze = cat_rhombus.instantiate()
-		self.add_child(rhombus_katze)
-		$AnimationPlayer.play("summon_car")
+	var cat_instance = load(Inventory.shape_to_cat(shape)).instantiate()
+	self.add_child(cat_instance)
+	$AnimationPlayer.play("summon_car")
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "summon_car":
