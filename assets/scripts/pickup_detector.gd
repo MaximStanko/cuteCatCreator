f99c298dae68100092915972f0065dca4ad27888
Nick <nick@henjes-net.de>
2026-06-21 17:46:44 +0200

    pickup sound

--- assets/scripts/pickup_detector.gd
+++ assets/scripts/pickup_detector.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 @export var pickupName: String
+@onready var item_sound: AudioStreamPlayer = $ItemPlayer
 
 var player_inside = false
 
@@ -17,9 +18,13 @@ func _on_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, l
 	if body.is_in_group("player"):
 		player_inside = false
 		$Sprite3D.hide()
+
+func _collect_item():
+	item_sound.play()
+	GameManager.player_pickup.emit(pickupName)
 		
 func _unhandled_key_input(event: InputEvent) -> void:
 	if not event.is_pressed() or event.is_echo() or !player_inside:
 		return
 	match (event as InputEventKey).physical_keycode:
-		KEY_E: GameManager.player_pickup.emit(pickupName) 
+		KEY_E: _collect_item()
